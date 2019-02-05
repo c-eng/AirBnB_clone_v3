02ffb387c72a66cd3d8559a68df6ddda41c69f9d
Stephen Chu <stephen.chu530@gmail.com>
2019-02-04 16:31:21 -0800

    feat: added stats return

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,20 @@
 """Routes for flask"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """/status response"""
     return jsonify(status="OK")
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Get stats"""
+    return jsonify({"amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")})
