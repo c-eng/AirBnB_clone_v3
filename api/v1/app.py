77ef2eff03007093d3a4c1c4560a0fd13e649acb
c-eng <ceng@cca.edu>
2019-02-05 00:54:54 +0000

    feat: Adding custom 404 route handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Comment here"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -13,6 +13,11 @@ app.config.update(JSONIFY_PRETTYPRINT_REGULAR=True)
 def teardown_appcontext(Exception):
     """close storage session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify(error="Not found"), 404
 
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,12 +10,13 @@ def status():
     """/status response"""
     return jsonify(status="OK")
 
+
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """Get stats"""
     return jsonify({"amenities": storage.count("Amenity"),
-            "cities": storage.count("City"),
-            "places": storage.count("Place"),
-            "reviews": storage.count("Review"),
-            "states": storage.count("State"),
-            "users": storage.count("User")})
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
