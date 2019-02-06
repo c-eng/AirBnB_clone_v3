b810de800ebf427a024da44159c951f495028c2f
Stephen Chu <stephen.chu530@gmail.com>
2019-02-06 12:54:52 -0800

    test: split out imports and used noqa flag

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,10 @@
 """Blueprint for Flask?"""
 from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-if app_views:
-    from api.v1.views.index import *
-    from api.v1.views import states, cities, amenities, users, places
-    from api.v1.views import places_reviews
+from api.v1.views.index import *  # noqa
+from api.v1.views import states  # noqa
+from api.v1.views import cities  # noqa
+from api.v1.views import amenities  # noqa
+from api.v1.views import users  # noqa
+from api.v1.views import places  # noqa
+from api.v1.views import places_reviews  # noqa
