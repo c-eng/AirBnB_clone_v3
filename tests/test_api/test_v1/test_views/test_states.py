0d6a5795778cea56167994b176283303a48b7bc1
c-eng <ceng@cca.edu>
2019-02-06 20:48:28 +0000

    feat: Adding temporary test for State view

--- tests/test_api/test_v1/test_views/test_states.py
+++ tests/test_api/test_v1/test_views/test_states.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python3
-'''Test states.py for expected behavior and documentation'''
+'''Test states view for expected behavior and documentation'''
 import unittest
+from flask import Flask
+from api.v1.app import app, storage
+from models.state import State
+
 
 class TestStates(unittest.TestCase):
-    '''Test states'''
+    '''Test class for states view'''
 
-    def test_something(self):
-        '''Test something'''
-        pass
+    @classmethod
+    def setUpClass(cls):
+        '''Set up flask'''
+        from api.v1.app import app
+        cls.app = app.test_client()
+        '''
+        cls.test_state = State(name="Test_State", id="7777777")
+        cls.test_state.save()
+        '''
+
+    def test_get(self):
+        '''Test GET on /states view'''
+        result = type(self).app.get('/api/v1/states/')
+        """
+        print(result.data.decode(encoding='UTF-8'))
+        """
+        self.assertEqual(result.status_code, 200)
