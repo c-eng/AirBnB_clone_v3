9747c9fc7b9a712e4d330c50142dbcea562d879f
c-eng <ceng@cca.edu>
2019-02-08 00:34:05 +0000

    feat: Adding route /places_search

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -56,7 +56,7 @@ def place(place_id=""):
             if meth == 'PUT':
                 data = request.get_json(silent=True)
                 if not data:
-                    return "Not a JSON", 400
+                    return jsonify("Not a JSON"), 400
                 for k, v in data.items():
                     if k not in ['id', 'user_id', 'city_id',
                                  'created_at', 'updated_at']:
@@ -64,3 +64,54 @@ def place(place_id=""):
                 p.save()
                 return jsonify(p.to_dict()), 200
     abort(404)
+
+
+@app_views.route('/places_search', methods=['POST'],
+                 strict_slashes=False)
+def place_search():
+    """Place search by state, city and/or amenity"""
+    data = request.get_json(silent=True)
+    if type(data) != dict:
+        return jsonify("Not a JSON"), 400
+    sid_list = data.get("states")
+    cid_list = data.get("cities")
+    aid_list = data.get("amenities")
+    cities_list = []
+    if sid_list:
+        for state_id in sid_list:
+            s = storage.get("State", state_id)
+            if s:
+                cities_list += s.cities
+            else:
+                return jsonify("Bad State"), 404
+    if cid_list:
+        for city_id in cid_list:
+            c = storage.get("City", city_id)
+            if c:
+                if c not in cities_list:
+                    cities_list.append(c)
+            else:
+                return jsonify("Bad City"), 404
+    place_list = []
+    if cities_list:
+        for city in cities_list:
+            place_list += city.places
+    else:
+        place_list = storage.all("Place").values()
+    if aid_list:
+        amenity_list = []
+        for amenity_id in aid_list:
+            a = storage.get("Amenity", amenity_id)
+            if a:
+                amenity_list.append(a)
+            else:
+                return jsonify("Bad Amenity"), 404
+        filter_list = []
+        for place in place_list:
+            if all(amenity in place.amenities for amenity in amenity_list):
+                pd = place.to_dict()
+                pd.pop('amenities')
+                filter_list.append(pd)
+        return jsonify(filter_list)
+    else:
+        return jsonify([p.to_dict() for p in place_list])
